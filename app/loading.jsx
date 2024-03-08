397b251b982b85a0e70f56ae1aa0c4766be40095
ANONYMOUS1601 <mankotiaayush131@gmail.com>
2024-03-08 17:56:12 +0530

    added loading component and global css

---
+++ app/loading.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <>
+    <div className="mt-80">
+    <section class="dots-container">
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+  <div class="dot"></div>
+</section>
+    </div>
+
+
+    </>
+  )
+}
+
+export default loading
