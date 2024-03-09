65681999b7d95b3a70868575cbaea23c1f0f4785
Aryainguz <aryaninguz369@gmail.com>
2024-03-09 12:12:39 +0530

    Add Faq component and configure next.config for image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['d33wubrfki0l68.cloudfront.net',
+            'cdn.rareblocks.xyz','bsmedia.business-standard.com','upload.wikimedia.org','imgs.search.brave.com'
+        ],
+    },
+};
 
 export default nextConfig;
