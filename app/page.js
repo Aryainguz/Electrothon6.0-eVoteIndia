8810c59b6816e7e41536e78308d2b6f7e615f97e
Aryainguz <aryaninguz369@gmail.com>
2024-03-09 14:11:30 +0530

    Add new components to Home page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import ContactForm from './components/ContactForm'
 import Feature from './components/Feature'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 import HowItWorks from './components/HowItWorks'
+import Navbar from './components/Navbar'
 import NewSec from './components/NewSec'
+import Faq from './components/Faq'
 
 export default function Home() {
   return (
@@ -13,6 +16,9 @@ export default function Home() {
       <Feature />
       <HowItWorks />
       <NewSec />
+      <Faq />
+      <ContactForm />
+      <Footer />
     </div>
   );
 }
