82f4aada292553d69ff05036bac236b9f1e3edfd
Aryainguz <aryaninguz369@gmail.com>
2024-03-09 19:19:50 +0530

    Update Hero and RegisterPage components

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
 
                             <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
                                 <div className="flex items-center">
-                                    <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">1.3 </p>
-                                    <p className="ml-3 text-sm text-gray-900 font-pj">Billion<br />Voters.</p>
+                                    <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">97 </p>
+                                    <p className="ml-3 text-sm text-gray-900 font-pj">Crore<br />Voters.</p>
                                 </div>
 
                                 <div className="hidden sm:block">
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -101,7 +101,7 @@ function RegisterPage() {
                 <label htmlFor className="text-xs font-semibold px-1" >First name</label>
                 <div className="flex">
                   <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg" /></div>
-                  <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John" onChange={
+                  <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Ram" onChange={
                   (e) => {
                     setFirstName(e.target.value)
                   }}
@@ -113,7 +113,7 @@ function RegisterPage() {
                 <label htmlFor className="text-xs font-semibold px-1">Last name</label>
                 <div className="flex">
                   <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg" /></div>
-                  <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Smith" onChange={
+                  <input type="text" className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Kumar" onChange={
                   (e) => {
                     setLastName(e.target.value)
                   }}
